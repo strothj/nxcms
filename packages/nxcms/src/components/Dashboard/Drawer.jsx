03e1006099799d734bb32f3ba0101feea07a3dd2
strothj <jason@jasons.io>
2017-05-17 19:44:24 -0400

    feature: add admin dashboard drawer links

--- packages/nxcms/src/components/Dashboard/Drawer.jsx
+++ packages/nxcms/src/components/Dashboard/Drawer.jsx
@@ -2,10 +2,14 @@ import Radium from 'radium';
 import React from 'react';
 import { connect } from 'react-redux';
 import Drawer from 'material-ui/Drawer';
+import Divider from 'material-ui/Divider';
 import Subheader from 'material-ui/Subheader';
 import MenuItem from 'material-ui/MenuItem';
 import HomeIcon from 'material-ui/svg-icons/action/home';
 import FaceIcon from 'material-ui/svg-icons/action/face';
+import TextFieldsIcon from 'material-ui/svg-icons/editor/text-fields';
+import PeopleIcon from 'material-ui/svg-icons/social/people';
+import SettingsIcon from 'material-ui/svg-icons/action/settings';
 
 import { actions } from 'store';
 import { breakpoints } from 'styles';
@@ -37,10 +41,21 @@ const DashboardDrawer = props => (
       <Link to="/dashboard/profile">
         <MenuItem leftIcon={<FaceIcon />}>Profile</MenuItem>
       </Link>
+      <MenuItem leftIcon={<TextFieldsIcon />}>Posts</MenuItem>
+      {props.profile.isAdmin &&
+        <div style={{ paddingTop: 32 }}>
+          <Divider />
+          <Subheader>Admin</Subheader>
+          <MenuItem leftIcon={<PeopleIcon />}>Editors</MenuItem>
+          <MenuItem leftIcon={<SettingsIcon />}>Site Settings</MenuItem>
+        </div>}
     </Drawer>
   </div>
 );
 
-const mapStateToProps = ({ showSideBar }) => ({ showSideBar });
+const mapStateToProps = ({ profile, showSideBar }) => ({
+  profile,
+  showSideBar,
+});
 
 export default connect(mapStateToProps)(Radium(DashboardDrawer));
